637572b842ab44db05e65bc855bcd0eec1a37870
profmilanez-fiap <pf1588@fiap.com.br>
2025-04-24 16:05:21 -0300

    Atualização CORS

--- src/main/java/br/com/fiap/filter/CorsFilter.java
+++ src/main/java/br/com/fiap/filter/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
